1a57c00c7b33e32791dfba0fe72eca88375e1338
Jatin Parmar <jatinrparmar2003@gmail.com>
2025-08-26 00:32:25 +0530

    firebase + 1

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYDjzJ_NetpZr-fCH5bTNKYCNO1X5_Nxc",
+  authDomain: "quizzer-21573.firebaseapp.com",
+  projectId: "quizzer-21573",
+  storageBucket: "quizzer-21573.firebasestorage.app",
+  messagingSenderId: "614038126483",
+  appId: "1:614038126483:web:bcb619fd42deb21898f266",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
